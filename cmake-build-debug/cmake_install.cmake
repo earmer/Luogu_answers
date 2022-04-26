905b3ae58876cf6ad1d6db656e20668db05c78c5
earmer <li15811183860@163.com>
2022-04-26 17:55:22 +0800

    Adjust a little bit of P1010.cpp

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/101/CLionProjects/Answersfor-luogu
+# Install script for directory: C:/Users/Work/CLionProjects/Luogu_answers
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/101/CLionProjects/Answersfor-luogu/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Work/CLionProjects/Luogu_answers/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- cpp/P1010.cpp
+++ cpp/P1010.cpp
@@ -8,25 +8,42 @@ int num[14] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
 bool ret[14] = {false};
 string s;
 
-void pt(){
-
-}
-
-void find(int n) {
-//    bool ret[14] = {false};
-    for (int i = 13; i >= 0; i--) {
-        if (n >= num[i]) {
-            ret[i] = true;
-            n -= num[i];
+int temp;
+int find(int n, int x)
+{
+    if (x == 1)
+        return temp;
+    //    bool ret[14] = {false};
+    for (int i = 13; i >= 0; i--)
+    {
+        if (n >= num[i])
+        {
+            temp = num[i];
+            return i;
         }
     }
 }
 
+void prt(int n)
+{
+    cout << "2";
+    if (n != 2)
+    {
+        int mi = find(n, 0);
+        int calculate = n - mi;
+        cout << "(" << prt(mi - 1) << ")";
+        if (n - find(n, 1))
+            cout << "+" << prt(n - find(n, 1));
+    }
+    if (n == 1)
+        cout << "(0)";
+}
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     find(n);
-    cout <<  pt();
+    cout << pt();
     return 0;
 }
